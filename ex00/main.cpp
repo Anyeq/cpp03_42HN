aa5788932d49fbfdb551d32a946786198e7bcb28
asando <alexando17@yahoo.com>
2026-07-05 15:36:47 +0200

    WIP: ex01 ScavTrap.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,23 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 15:25:14 by asando            #+#    #+#             */
-/*   Updated: 2026/06/30 15:25:16 by asando           ###   ########.fr       */
+/*   Updated: 2026/07/05 14:40:52 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int	main(void) {
+	ClapTrap a("Giant");
+	ClapTrap b("Enemy");
+
+	a.attack("Enemy");
+	b.takeDamage(5);
+
+	b.attack("Giant");
+	a.takeDamage(3);
+	
+	a.beRepaired(4);
+	b.beRepaired(2);
+	return (0);
+}
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,7 +6,26 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 14:44:48 by asando            #+#    #+#             */
-/*   Updated: 2026/07/05 14:44:51 by asando           ###   ########.fr       */
+/*   Updated: 2026/07/05 15:36:22 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+# include <string>
+
+class	ScavTrap : public ClapTrap {
+	public:
+		ScavTrap();
+		ScavTrap(const std::string name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap& operator=(const ScavTrap& rhs);
+		~ScavTrap();
+
+		void	attack(const std::string& target);
+		void	guardGate();
+
+};
+#endif
