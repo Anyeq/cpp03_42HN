12760bea041397dc43406824aa866db4e5b5745c
asando <alexando17@yahoo.com>
2026-07-05 16:20:35 +0200

    ADD: Functions in ScavTrap.cpp

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,58 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 14:45:03 by asando            #+#    #+#             */
-/*   Updated: 2026/07/05 14:45:05 by asando           ###   ########.fr       */
+/*   Updated: 2026/07/05 16:20:07 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScavTrap.hpp"
+#include <iostream>
+#include <string>
+
+ScavTrap::ScavTrap() : ClapTrap("DefaultScav") {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap Default constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap constructor called for " << _name << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other) {
+	std::cout << "ScavTrap copy constructor called" << std::endl;
+	*this = other;
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& rhs) {
+	std::cout << "ScavTrap assignment operator called" << std::endl;
+	if (this != &rhs) {
+		ClapTrap::operator=(rhs);
+	}
+	return (*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap destructor called for " << _name << std::endl;
+}
+
+void	ScavTrap::attack(const std::string& target) {
+	if (_energyPoints <= 0 || _hitPoints <= 0) {
+		std::cout << "ScavTrap " << _name << " cannot attack!" << std::endl;
+		return;
+	}
+	_energyPoints--;
+	std::cout << "ScavTrap " << _name << " attacks " << target
+		<< ", causing " << _attackDamage << " points of damage!"
+		<< std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << _name << " is now in Gate Keeper mode!" << std::endl;
+}
