7c9694147af98976708e73296b7f0e83728512b9
asando <alexando17@yahoo.com>
2026-07-08 14:19:51 +0200

    ADd: main function DiamondTrap

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,26 +6,21 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 15:25:14 by asando            #+#    #+#             */
-/*   Updated: 2026/07/05 16:21:01 by asando           ###   ########.fr       */
+/*   Updated: 2026/07/08 14:19:36 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScavTrap.hpp"
+#include "DiamondTrap.hpp"
 
-int main(void)
+int	main(void)
 {
-    ScavTrap a("Alex");
-    ScavTrap b("Enemy");
+	DiamondTrap d("Alex");
 
-    a.attack("Enemy");
-    b.takeDamage(20);
+	d.attack("Enemy");
+	d.whoAmI();
 
-    b.attack("Alex");
-    a.takeDamage(5);
+	d.takeDamage(20);
+	d.beRepaired(10);
 
-    a.beRepaired(10);
-
-    a.guardGate();
-
-    return 0;
+	return 0;
 }
