6fe3b7db055a82fbda74efae48b5783f3eb25cde
asando <alexando17@yahoo.com>
2026-08-21 14:22:46 +0200

    ADD: more detail main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,26 +6,37 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 15:25:14 by asando            #+#    #+#             */
-/*   Updated: 2026/07/05 16:21:01 by asando           ###   ########.fr       */
+/*   Updated: 2026/08/21 11:07:41 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main(void)
 {
-    ScavTrap a("Alex");
-    ScavTrap b("Enemy");
+    ScavTrap scavA("scavObject");
+    ScavTrap scavB("scavObjectEnemy");
 
-    a.attack("Enemy");
-    b.takeDamage(20);
+	FragTrap fragX("fragObject");
+	FragTrap fragY("fragObjectEnemy");
 
-    b.attack("Alex");
-    a.takeDamage(5);
+    scavA.attack("scavObjectEnemy");
+    scavB.takeDamage(20);
+    scavB.attack("scavObject");
+    scavA.takeDamage(5);
 
-    a.beRepaired(10);
 
-    a.guardGate();
+    fragX.attack("fragObjectEnemy");
+    fragY.takeDamage(20);
+    fragY.attack("fragObject");
+    fragX.takeDamage(5);
+
+    scavA.beRepaired(10);
+	fragX.beRepaired(20);
+
+    scavA.guardGate();
+	fragX.highFivesGuys();
 
     return 0;
 }
